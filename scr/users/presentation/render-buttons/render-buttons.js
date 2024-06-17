ee370d88355eb5f53df99d53e102397b5ab11662
Israel Moreno <yxrael@gmail.com>
2024-06-17 15:37:14 +0200

    botones

--- scr/users/presentation/render-buttons/render-buttons.js
+++ scr/users/presentation/render-buttons/render-buttons.js
@@ -1,9 +1,11 @@
 
 import usersStore from '../../store/users-store';
+import { renderTable } from '../render-table/render-table';
 import './render-buttons.css';
 
 
 export const renderButtons = ( element ) => {
+
     const nextButton = document.createElement('button');
     nextButton.innerText = ' Next >';
 
@@ -15,4 +17,18 @@ export const renderButtons = ( element ) => {
     currentPageLabel.innerText = usersStore.getCurrentPage();
 
     element.append( prevButton, currentPageLabel, nextButton );
+
+    nextButton.addEventListener('click', async() => {
+        await usersStore.loadNextPage();
+        currentPageLabel.innerText = usersStore.getCurrentPage();
+        renderTable( element );
+    });
+
+    prevButton.addEventListener('click', async() => {
+        await usersStore.loadPreviousPage();
+        currentPageLabel.innerText = usersStore.getCurrentPage();
+        renderTable( element );
+    })
 }
+
+
--- scr/users/users-app.js
+++ scr/users/users-app.js
@@ -1,4 +1,6 @@
+import { renderAddButton } from "./presentation/render-add-button/render-add-button";
 import { renderButtons } from "./presentation/render-buttons/render-buttons";
+import { renderModal } from "./presentation/render-modal/render-modal";
 import { renderTable } from "./presentation/render-table/render-table";
 import usersStore from "./store/users-store";
 
@@ -19,6 +21,8 @@ export const UsersApp = async ( element ) => {
 
     renderTable( element );
     renderButtons( element );
+    renderAddButton( element );
+    renderModal( element );
 
     
  
